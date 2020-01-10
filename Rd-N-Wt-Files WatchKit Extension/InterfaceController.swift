ac9c0847e19083d6fbcca7ad24f50a8724410dd6
Armandocarrillo <ingcompuarmando@gmail.com>
2020-01-09 23:12:03 -0600

    url files

--- Rd-N-Wt-Files WatchKit Extension/InterfaceController.swift
+++ Rd-N-Wt-Files WatchKit Extension/InterfaceController.swift
@@ -14,9 +14,23 @@ class InterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        testFile()
+        testFile()
         
         // Configure interface objects here.
     }
+    func getDocumentsDirectory() -> URL { let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+       return paths[0]
+    }
+    
+    func testFile() { let saveURL = getDocumentsDirectory().appendingPathComponent("shared_file")
+        if let contents = try? String(contentsOf: saveURL) {//intenta cargar el la cadena, si se puede leer ejecutar :
+        print("Reading from file")
+        print(contents)
+           } else {
+        print("Writing to file")
+        try? "This is the saved file".write(to: saveURL, atomically: true, encoding: String.Encoding.utf8)//escribe en una url, codifica con String.Encoding.utf8
+        } }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
